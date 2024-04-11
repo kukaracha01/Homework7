1fa63e8371c9a115b75c590906e1b65f414b7069
mu <semkadarov@gmail.com>
2024-04-11 21:58:11 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,17 @@ public class Main {
             allMoneyInBank = allMoneyInBank + allMoneyInBank / 100;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + allMoneyInBank + " рублей");
         }
+
+        System.out.println("Task1");
+        summInMonth = 15000;
+        allMoneyInBank = 0;
+        int month = 1;
+        while (allMoneyInBank <= 2_459_000) {
+            allMoneyInBank = allMoneyInBank + summInMonth;
+            allMoneyInBank = allMoneyInBank + allMoneyInBank / 100;
+            month = month + 1;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + allMoneyInBank + " рублей");
+        }
+
     }
 }
